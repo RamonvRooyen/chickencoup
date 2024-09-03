c4cc2c2cf4522a86124b9bee5f6c4a311c91d90f
Ramon <ramon@eastbridge.eu>
2024-09-03 21:44:34 +0200

    first files

---
+++ src/button.h
@@ -0,0 +1,26 @@
+#include <optional>
+#include <Arduino.h>
+
+#if defined(__arm__) || defined(ESP8266) || defined(ESP32)
+    #include <functional>
+    using fptr = std::function<void()>;
+#else
+    typedef void (*fptr)();
+#endif
+
+
+class Button{
+    public: 
+        Button(int pin);
+        void onDown(fptr callback);
+        void onUp(fptr callback);
+        void loop();
+        bool isPressed();
+        bool debug = false;
+
+    private:
+        bool buttonPressed = false;
+        int pin;
+        std::optional<fptr> downCallback;
+        std::optional<fptr> upCallback;
+};
